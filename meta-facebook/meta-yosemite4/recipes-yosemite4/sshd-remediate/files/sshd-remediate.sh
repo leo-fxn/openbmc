448b7c0dd85decba3a625a6e8e60eedaba10fa13
Pete O_o <pmb@meta.com>
2025-01-28 16:28:36 -0800

    Log remediations to SEL
    Summary: When running the sshd remediation, we should be logging to the SEL.
    
    Test Plan:
    Killed `sshd.socket` and allowed the remediation to kick in. After reboot, I checked the logs
    
    {P1720797225}
    
    Reviewed By: williamspatrick
    
    Differential Revision: D68803595
    
    fbshipit-source-id: 619c71062871a884fcbeaa853c27ec85aae84b38

--- meta-facebook/meta-yosemite4/recipes-yosemite4/sshd-remediate/files/sshd-remediate.sh
+++ meta-facebook/meta-yosemite4/recipes-yosemite4/sshd-remediate/files/sshd-remediate.sh
@@ -5,6 +5,15 @@ FAIL_STAMP=/run/ssh_failed
 FAIL_THRESHOLD=$((60 * 15))
 
 
+function log_sel {
+    MESSAGE="$1"
+    echo "$MESSAGE"
+    busctl call \
+    xyz.openbmc_project.Logging /xyz/openbmc_project/logging \
+    xyz.openbmc_project.Logging.Create Create "ssa{ss}" "$MESSAGE" \
+    xyz.openbmc_project.Logging.Entry.Level.Error 0
+}
+
 function fail_ssh_check {
   if [[ ! -f "$FAIL_STAMP" ]]; then
     touch $FAIL_STAMP
@@ -12,7 +21,7 @@ function fail_ssh_check {
   AGE=$(($(date +%s) - $(stat -c %Y "$FAIL_STAMP")))
   echo "Failing for $AGE seconds"
   if [[ $AGE -gt $FAIL_THRESHOLD ]]; then
-    echo "Rebooting OOB due to continued ssh banner failure"
+    log_sel "[sshd-remediate] Rebooting OOB due to continued ssh banner failure"
     /usr/sbin/reboot
   fi
   exit 1
