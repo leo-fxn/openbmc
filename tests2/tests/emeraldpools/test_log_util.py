65bf9d6d04f953063b554a1f93997c7386d3b862
Ciaran Concannon <ciaranconcannon@meta.com>
2025-04-01 06:31:36 -0700

    Fix emeraldpools CIT tests
    Summary:
    Emeraldpools CIT tests were never defined properly, now that we have test capacity it is [failing 10 and skipping 7](https://fburl.com/conveyor/uv4l3u64)
    
    Let's fix so we are actually testing what we should be:
    - Update running processes test to look for dhcpcd instead of dhclient
    - Ignore eth0 v6 test, but add one for usb0
    - Set the expected fru names and IDs for log_util test, remove test skip for `log-util --clear`
    
    Test Plan:
    Test run - all tests pass, only two tests skipped are the eth0 tests, not appropriate for this platform
    ```
    [ciaranconcannon@devvm24298.cln0 ~/local/openbmc (zion_cit)]$ HOST=rtptest1649-oob1.ftw6.facebook.com
    [ciaranconcannon@devvm24298.cln0 ~/local/openbmc (zion_cit)]$ PLATFORM=emeraldpools
    [ciaranconcannon@devvm24298.cln0 ~/local/openbmc (zion_cit)]$ ~/local/openbmc/tests2/openbmc_tester.py --hostname $HOST --port 22 --password $(rotor2 get-credential openbmc root@$HOST
    pw | awk '{print $2}') --platform $PLATFORM
    ...
    ...
    ...
    ----------------------------------------------------------------------
    Ran 47 tests in 124.767s
    
    OK (skipped=2)
    ```
    
    Reviewed By: kawmarco
    
    Differential Revision: D72107885
    
    fbshipit-source-id: db04d8a55be565fdc9afabe2e430ebbac586f397

--- tests2/tests/emeraldpools/test_log_util.py
+++ tests2/tests/emeraldpools/test_log_util.py
@@ -22,32 +22,74 @@ import unittest
 from common.base_log_util_test import BaseLogUtilTest
 
 
+FRU_IDS = {
+    "mb": 1,
+    "pdb": 2,
+    "bsm": 3,
+    "asic0": 4,
+    "asic1": 5,
+    "asic2": 6,
+    "asic3": 7,
+    "asic4": 8,
+    "asic5": 9,
+    "asic6": 10,
+    "asic7": 11,
+}
+
+
 class AllLogUtilTest(BaseLogUtilTest, unittest.TestCase):
     FRU = "all"
 
-    @unittest.skip("not available")
-    def test_log_clear(self):
-        pass
+    def set_fru_id(self):
+        """
+        for spacific FRU need to override this function
+        to define FRU_ID with fru id number by integer type
+        """
+        self.FRU_ID = FRU_IDS.get(self.FRU, None)
 
 
 class MbLogUtilTest(AllLogUtilTest):
     FRU = "mb"
 
 
-class NicLogUtilTest(AllLogUtilTest):
-    FRU = "nic"
+class Asic0LogUtilTest(AllLogUtilTest):
+    FRU = "asic0"
 
 
-class Riserslot2LogUtilTest(AllLogUtilTest):
-    FRU = "riser_slot2"
+class Asic1LogUtilTest(AllLogUtilTest):
+    FRU = "asic1"
 
 
-class Riserslot3LogUtilTest(AllLogUtilTest):
-    FRU = "riser_slot3"
+class Asic2LogUtilTest(AllLogUtilTest):
+    FRU = "asic2"
 
 
-class Riserslot4LogUtilTest(AllLogUtilTest):
-    FRU = "riser_slot4"
+class Asic3LogUtilTest(AllLogUtilTest):
+    FRU = "asic3"
+
+
+class Asic4LogUtilTest(AllLogUtilTest):
+    FRU = "asic4"
+
+
+class Asic5LogUtilTest(AllLogUtilTest):
+    FRU = "asic5"
+
+
+class Asic6LogUtilTest(AllLogUtilTest):
+    FRU = "asic6"
+
+
+class Asic7LogUtilTest(AllLogUtilTest):
+    FRU = "asic7"
+
+
+class BsmLogUtilTest(AllLogUtilTest):
+    FRU = "bsm"
+
+
+class PdbLogUtilTest(AllLogUtilTest):
+    FRU = "pdb"
 
 
 class SysLogUtilTest(BaseLogUtilTest, unittest.TestCase):
