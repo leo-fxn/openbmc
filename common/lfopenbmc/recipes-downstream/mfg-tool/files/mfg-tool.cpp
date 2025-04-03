4f00ccd7d069be66524e2680b10cd43d10055160
Pete O_o <pmb@meta.com>
2025-04-02 21:10:53 -0700

    Add some retry and timeout logic
    Summary:
    This way if RTP wants to have retries, they can create a dotfile under `/home/root/.mfgtool.json`. This file is json file with key value pairs. Currently it support `retries` and `timeout`
    
    If the command fails to execute, it will try again and again for the number defined in `retries`. It will exit once the rc of the child process exits cleanly, or the number of retries have been exhausted.
    
    There's also a timeout feature. This simply creates an `alarm` signal and the child process is killed. The retries is honoured.
    
    Test Plan:
    Ran this on an actual BMC, populating the json file with timeouts and retry values. Watched it timeout appropriately as well as retried when it failed
    {P1778970450}
    
    Testing return codes
    ```
    root@sled327803164-oob:~# /tmp/mfg-tool hello ; echo $?
    <7> Hello World!
    [
        "Hello World!"
    ]
    0
    root@sled327803164-oob:~# /tmp/mfg-tool helloXXX ; echo $?
    A subcommand is required
    Run with --help for more information.
    106
    ```
    
    Reviewed By: williamspatrick
    
    Differential Revision: D72364358
    
    fbshipit-source-id: 05c552c2ad7f3d3658ba3f9e9c702cc132cf7fa0

--- common/lfopenbmc/recipes-downstream/mfg-tool/files/mfg-tool.cpp
+++ common/lfopenbmc/recipes-downstream/mfg-tool/files/mfg-tool.cpp
@@ -1,8 +1,16 @@
+#include "utils/json.hpp"
 #include "utils/register.hpp"
+#include "utils/supervise.hpp"
 
 #include <CLI/CLI.hpp>
 
 #include <algorithm>
+#include <format>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 int main(int argc, char** argv)
 {
@@ -10,6 +18,27 @@ int main(int argc, char** argv)
     app.require_subcommand(1);
     mfgtool::init_commands(app);
 
-    CLI11_PARSE(app, argc, argv);
-    return 0;
+    int timeout = 0;
+    int retries = 0;
+    std::string configFN = std::format("{}/.mfgtool.json", getenv("HOME"));
+
+    if (std::filesystem::exists(configFN))
+    {
+        std::ifstream ifs(configFN);
+        auto config = mfgtool::js::parse(ifs);
+
+        if (auto iter = config.find("timeout"); iter != config.end())
+        {
+            timeout = config["timeout"];
+        }
+        if (auto iter = config.find("retries"); iter != config.end())
+        {
+            retries = config["retries"];
+        }
+    }
+    auto _ = [&]() {
+        CLI11_PARSE(app, argc, argv);
+        return 0;
+    };
+    return mfg_tool::supervise(_, timeout, retries);
 }
---
+++ common/lfopenbmc/recipes-downstream/mfg-tool/files/utils/supervise.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <functional>
+
+namespace mfg_tool
+{
+PHOSPHOR_LOG2_USING;
+
+static pid_t pid;
+static int was_killed = 0;
+
+void alarmHandler(int signum)
+{
+    was_killed = 1;
+    kill(pid, SIGTERM);
+    error("Alarm signal {SIGNUM} received. Child process timed out", "SIGNUM",
+          signum);
+}
+
+int supervise(std::function<int()> fn, int timeout, int retries)
+{
+    int es = 0;
+    for (int attempt = 0; attempt <= retries; attempt++)
+    {
+        was_killed = 0;
+        pid = fork();
+        if (pid == 0)
+        {
+            exit(fn());
+        }
+        else if (pid > 0)
+        {
+            signal(SIGALRM, alarmHandler);
+            alarm(timeout);
+            int status;
+            wait(&status);
+            es = WEXITSTATUS(status);
+            alarm(0);
+            if (was_killed)
+            {
+                exit(137);
+            }
+        }
+    }
+    return es;
+}
+} // namespace mfg_tool
