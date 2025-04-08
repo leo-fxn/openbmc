c4d4cabd6adc918c6394d2249e662f6804ef07fe
neal_chen <neal_chen@accton.com>
2025-04-08 11:35:50 -0700

    minipack3n: show-tech: implement show-tech
    Summary:
    # Description
    Add show-tech to display dump value
    1. bmc versrion
    2. bmc revision
    3. fru eeprom
    4. boot log
    5. flash info
    6. oob status
    7. i2cdetect
    8. network interface
    9. sensors
    10. gpio info
    
    # Motivation
    Add show-tech to dump messages for debuging
    
    X-link: https://github.com/facebookexternal/openbmc.accton/pull/206
    
    Test Plan:
    "bitbake minipack3n-image" succeeded
    
    Run "show_test.py -v"
    [show_tech.log](https://github.com/user-attachments/files/19641381/show_tech.log)
    
    Reviewed By: mikechoifb
    
    Differential Revision: D72578117
    
    Pulled By: tao-ren
    
    fbshipit-source-id: 119c963d1fc9d3929fee5c0a6fb2868d1256821c

--- meta-facebook/meta-minipack3n/recipes-utils/show-tech/files/bmc_board_rev.sh
+++ meta-facebook/meta-minipack3n/recipes-utils/show-tech/files/bmc_board_rev.sh
@@ -1,4 +1,6 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+#!/bin/bash
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates. (http://www.meta.com)
 #
 # This program file is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -14,10 +16,10 @@
 # Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
+#
 
-require recipes-core/images/fboss-lite-image.inc
+# shellcheck disable=SC1091
+# read the Hardware revision from MCB CPLD register
+. /usr/local/bin/openbmc-utils.sh
 
-IMAGE_INSTALL += " \
-    host-recovery \
-    show-tech \
-    "
+wedge_board_rev
