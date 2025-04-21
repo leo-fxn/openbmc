8fd018c8221d40a05665e16c36cc7fbceb65fb0e
Patrick Williams <patrickw3@meta.com>
2025-04-20 21:39:40 -0700

    yocto: update master:c5fb130e6
    Summary:
    Update master at c5fb130e6
    update-submodule: yocto/lf-master
    
    - Rebase dbus-sensors patches.
    - Rebase Yv4 entity-manager patches.
    
    Test Plan: CIT build passes.
    
    Reviewed By: jamesatha
    
    Differential Revision: D73348656
    
    fbshipit-source-id: ca69318699632e5ec82b202b70536a1c43cb88ac

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:5fe4f9bab
+    lf-openbmc:c5fb130e6
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b
