15246b25956950e4996d68483de0b71b03c0b559
Pete O_o <pmb@meta.com>
2025-05-11 17:43:59 -0700

    yocto: update master:d5f1953c5
    Summary:
    Update master at d5f1953c5
    update-submodule: yocto/lf-master
    
    - Rebased `pldm` patches
    
    Test Plan: CIT build passes.
    
    Reviewed By: jagpalgill
    
    Differential Revision: D74546224
    
    fbshipit-source-id: 1ec3d19536fc0fe7e3a688d499b1919539f8fcea

--- yocto_repos.sh
+++ yocto_repos.sh
@@ -12,7 +12,7 @@ rocko_repos=(
 )
 rocko_poky_patch="0001-rocko-backport-support-for-override.patch 0002-Remove-checks-on-python.patch 0003-Add-support-to-build-on-centOS9.patch"
 lf_master_repos=(
-    lf-openbmc:fd47d0194
+    lf-openbmc:d5f1953c5
 )
 lf_dunfell_repos=(
     lf-openbmc:c2858f16b
